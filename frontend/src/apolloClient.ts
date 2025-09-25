fe20f427fc26f121d152faa40c1c19b538f0398a
Priya_P <124154881+Priyap1038@users.noreply.github.com>
2025-09-25 17:18:10 +0530

    Completd frontend setup

---
+++ frontend/src/apolloClient.ts
@@ -0,0 +1,11 @@
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "http://127.0.0.1:8000/graphql/",
+    headers: { "X-ORG-SLUG": "acme" },
+  }),
+  cache: new InMemoryCache(),
+});
+
+export default client;
